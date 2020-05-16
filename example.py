ae0d3b5b30a9b008a0cfb18cfab508b02e36c296
rahul <rahulpramod.r@gmail.com>
2020-05-16 11:45:41 +0530

    Commented 2 lines

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
-print("Welcome to example.py")
+#print("Welcome to example.py")
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
- print("End of the program")
+# print("End of the program")
 git_opeation()
