2229016111b2f8cc8fda999e3708ffda4ea15b38
Rahul Pramod <rahulpramod.r@gmail.com>
2020-05-20 20:23:04 +0530

    Edited text directly from gitbash, now pushing it from gitbash

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
- print("Testing Push from pycharm")
+ print("Testing Push from pycharm, now testing push from gitbash")
 git_opeation()
